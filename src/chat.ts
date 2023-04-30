7fc325658f3c034cc5f6432f920a36a7394ef3fe
eumemic <eumemic@gmail.com>
2023-04-29 21:25:42 -0700

    use ConversationChain

--- src/chat.ts
+++ src/chat.ts
@@ -1,17 +1,37 @@
+import { ConversationChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models/openai";
+import { BufferMemory } from "langchain/memory";
 import {
-  AIChatMessage,
-  HumanChatMessage,
-  SystemChatMessage,
-} from "langchain/schema";
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  MessagesPlaceholder,
+  SystemMessagePromptTemplate,
+} from "langchain/prompts";
+import { last } from "lodash";
 import { getEnv } from "./env";
 import { SignalEvent, SignalGroup, sendMessage } from "./signal-api";
 import TaskQueue from "./task-queue";
 
+const { openAIApiKey, agentName, agentPhoneNumber } = getEnv();
+
 export class Chat {
   private taskQueue = new TaskQueue();
   private messages: Message[] = [];
-  private pending = false;
+  private chain = new ConversationChain({
+    memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
+    prompt: ChatPromptTemplate.fromPromptMessages([
+      SystemMessagePromptTemplate.fromTemplate(
+        `You are a helpful and friendly assistant named ${agentName}. You are on a first name basis with everyone in the chat. You always answer factually to the best of your ability and never make things up. Some of the conversations in the chat do not involve you; if you are not being addressed should respond with exactly this as the entirety of your response: "${NO_RESPONSE}". It costs money when you respond, so use your best judgement.`
+      ),
+      new MessagesPlaceholder("history"),
+      HumanMessagePromptTemplate.fromTemplate("{input}"),
+    ]),
+    llm: new ChatOpenAI({
+      openAIApiKey: openAIApiKey,
+      temperature: 0,
+      modelName: "gpt-3.5-turbo",
+    }),
+  });
 
   constructor(private chatId: string, private group?: SignalGroup) {}
 
@@ -27,103 +47,57 @@ export class Chat {
     this.taskQueue.runPeriodically(this.processEvents.bind(this), 1000);
   }
 
-  addEvent({ envelope }: SignalEvent) {
-    const { sourceNumber, sourceName, dataMessage, timestamp } = envelope;
-    this.messages.push({
-      sourceNumber,
-      sourceName,
-      timestamp,
-      content: dataMessage!.message!,
-    });
-    this.pending = true;
+  addEvent({
+    envelope: { sourceNumber, sourceName, dataMessage, timestamp },
+  }: SignalEvent) {
+    const content = dataMessage!.message!;
+    const lastMessage = last(this.messages);
+    if (lastMessage && lastMessage.sourceNumber === sourceNumber) {
+      lastMessage.content += `\n${content}`;
+    } else {
+      this.messages.push({
+        sourceNumber,
+        sourceName,
+        timestamp,
+        content,
+      });
+    }
   }
 
   private async processEvents() {
-    if (!this.pending) return;
-    this.pending = false;
+    const { chatId, group, messages, chain } = this;
+    if (!messages.length) return;
 
-    const { openAIApiKey, agentName, agentPhoneNumber } = getEnv();
-    const { chatId, group, messages } = this;
-
-    const chatMessages = [
-      new SystemChatMessage(
-        `You are a helpful and friendly assistant named ${agentName}. You are on a first name basis with everyone in the chat. You always answer factually to the best of your ability and never make things up. Some of the conversations in the chat do not involve you; if you are not being addressed should respond with exactly this as the entirety of your response: "${NO_RESPONSE}". It costs money when you respond, so use your best judgement.`
-      ),
-      ...messages
-        .sort((a, b) => a.timestamp - b.timestamp)
-        .map(({ sourceNumber, sourceName, content }) => {
-          const message =
-            sourceNumber === agentPhoneNumber
-              ? new AIChatMessage(content)
-              : new HumanChatMessage(
-                  `FROM: ${sourceName}\nMESSAGE: ${content}`
-                );
-          // message.name = sourceNumber;
-          return message;
-        }),
-      // new HumanChatMessage(formatChatMessage(agentName)),
-    ];
-
-    // this.log(
-    //   `\n===\n`,
-    //   JSON.stringify(
-    //     chatMessages.map((msg) => ({
-    //       source: msg.name,
-    //       message: msg.text,
-    //     })),
-    //     null,
-    //     2
-    //   ),
-    //   "\n==="
-    // );
-
-    const model = new ChatOpenAI({
-      openAIApiKey,
-      temperature: 0,
-      modelName: "gpt-3.5-turbo",
-    });
-
-    this.log(`Reflecting on ${chatMessages.length} messages...`);
+    this.log("Reflecting...");
 
     const t0 = Date.now();
 
-    let agentMessage: string;
+    const responses = (await chain.apply(
+      messages.map(({ sourceName, content }) => ({
+        input: `FROM: ${sourceName}\n---\n${content}`,
+      }))
+    )) as { response: string }[];
 
-    try {
-      const response = await model.call(chatMessages);
-      agentMessage = response.text;
-    } catch (e) {
-      console.error("Error while consulting LLM", e);
-      return;
-    }
+    // this.log("memory", (chain.memory as any).chatHistory.messages);
 
-    this.log(`Decided after ${((Date.now() - t0) / 1000).toFixed(1)}s`);
+    for (const { response } of responses) {
+      if (response === NO_RESPONSE) {
+        this.log("Chose not to respond");
+        continue;
+      }
 
-    let timestamp: number | undefined;
-    if (agentMessage === NO_RESPONSE) {
-      this.log("No response");
-      timestamp = Date.now();
-    } else {
-      timestamp = await sendMessage({
+      this.log("Responding...");
+
+      await sendMessage({
         number: agentPhoneNumber,
         recipients: [group?.id || chatId],
-        message: agentMessage,
+        message: response,
       });
     }
 
-    if (!timestamp) {
-      this.log(`No timestamp receieved`);
-      return;
-    }
+    messages.length = 0;
 
-    messages.push({
-      sourceNumber: agentPhoneNumber,
-      sourceName: agentName,
-      timestamp,
-      content: agentMessage,
-    });
-
-    this.log(`Responded`);
+    this.log(`Finished after ${((Date.now() - t0) / 1000).toFixed(1)}s`);
   }
 
   private log(...params: any[]) {
