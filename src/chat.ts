e2b4f6197973a6bebb75a7f249facedd0ed766fa
eumemic <eumemic@gmail.com>
2023-04-29 21:25:42 -0700

    separate single chat logic into its own module, run independently

---
+++ src/chat.ts
@@ -0,0 +1,139 @@
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import {
+  AIChatMessage,
+  HumanChatMessage,
+  SystemChatMessage,
+} from "langchain/schema";
+import { getEnv } from "./env";
+import { SignalEvent, SignalGroup, sendMessage } from "./signal-api";
+import TaskQueue from "./task-queue";
+
+export class Chat {
+  private taskQueue = new TaskQueue();
+  private messages: Message[] = [];
+  private pending = false;
+
+  constructor(private chatId: string, private group?: SignalGroup) {}
+
+  static getId({ envelope }: SignalEvent): string | undefined {
+    2;
+    const { sourceNumber, sourceName, dataMessage, timestamp } = envelope;
+    if (!dataMessage || !dataMessage.message) return;
+    return dataMessage.groupInfo?.groupId || sourceNumber;
+  }
+
+  start() {
+    this.taskQueue.runPeriodically(() => this.processEvents(), 1000);
+    return this;
+  }
+
+  addEvent({ envelope }: SignalEvent) {
+    const { sourceNumber, sourceName, dataMessage, timestamp } = envelope;
+    this.messages.push({
+      sourceNumber,
+      sourceName,
+      timestamp,
+      content: dataMessage!.message!,
+    });
+    this.pending = true;
+  }
+
+  private async processEvents() {
+    if (!this.pending) return;
+    this.pending = false;
+
+    const { openAIApiKey, agentName, agentPhoneNumber } = getEnv();
+    const { chatId, group, messages } = this;
+
+    const chatMessages = [
+      new SystemChatMessage(
+        `You are a helpful and friendly assistant named ${agentName}. You are on a first name basis with everyone in the chat. You always answer factually to the best of your ability and never make things up. Some of the conversations in the chat do not involve you; if you are not being addressed should respond with exactly this as the entirety of your response: "${NO_RESPONSE}". It costs money when you respond, so use your best judgement.`
+      ),
+      ...messages
+        .sort((a, b) => a.timestamp - b.timestamp)
+        .map(({ sourceNumber, sourceName, content }) => {
+          const message =
+            sourceNumber === agentPhoneNumber
+              ? new AIChatMessage(content)
+              : new HumanChatMessage(
+                  `FROM: ${sourceName}\nMESSAGE: ${content}`
+                );
+          // message.name = sourceNumber;
+          return message;
+        }),
+      // new HumanChatMessage(formatChatMessage(agentName)),
+    ];
+
+    // this.log(
+    //   `\n===\n`,
+    //   JSON.stringify(
+    //     chatMessages.map((msg) => ({
+    //       source: msg.name,
+    //       message: msg.text,
+    //     })),
+    //     null,
+    //     2
+    //   ),
+    //   "\n==="
+    // );
+
+    const model = new ChatOpenAI({
+      openAIApiKey,
+      temperature: 0,
+      modelName: "gpt-3.5-turbo",
+    });
+
+    this.log(`Thinking...`);
+
+    let agentMessage: string;
+
+    try {
+      const response = await model.call(chatMessages);
+      agentMessage = response.text;
+    } catch (e) {
+      console.error("Error while consulting LLM", e);
+      return;
+    }
+
+    this.log(`Decided`);
+
+    let timestamp: number | undefined;
+    if (agentMessage === NO_RESPONSE) {
+      this.log("No response");
+      timestamp = Date.now();
+    } else {
+      timestamp = await sendMessage({
+        number: agentPhoneNumber,
+        recipients: [group?.id || chatId],
+        message: agentMessage,
+      });
+    }
+
+    if (!timestamp) {
+      this.log(`No timestamp receieved`);
+      return;
+    }
+
+    messages.push({
+      sourceNumber: agentPhoneNumber,
+      sourceName: agentName,
+      timestamp,
+      content: agentMessage,
+    });
+
+    this.log(`Responded`);
+  }
+
+  private log(...params: any[]) {
+    console.log(`[${this.chatId}]`, ...params);
+  }
+}
+
+interface Message {
+  sourceNumber: string;
+  sourceName: string;
+  timestamp: number;
+  content: string;
+}
+
+const NO_RESPONSE = "%__NO_RESPONSE__%";
--- src/main.ts
+++ src/main.ts
@@ -13,12 +13,14 @@ import {
 } from "./signal-api";
 import { sleep } from "./util";
 import { getEnv } from "./env";
+import { Chat } from "./chat";
 
 main();
 
 async function main() {
   const { openAIApiKey, agentName, agentPhoneNumber } = getEnv();
 
+  const chats = new Map<string, Chat>();
   const groupLookup = new Map<string, SignalGroup>();
 
   while (true) {
@@ -30,148 +32,24 @@ async function main() {
 
       for (const group of signalGroups) {
         if (groupLookup.has(group.internal_id)) continue;
-
         console.log(`Added to group ${group.name}`);
         groupLookup.set(group.internal_id, group);
       }
 
-      // const groupLookup = keyBy(signalGroups, (group) => group.internal_id);
+      for (const event of signalEvents) {
+        const chatId = Chat.getId(event);
+        if (!chatId) continue;
 
-      // if (signalEvents.length) console.log(JSON.stringify(signalEvents, null, 2));
-
-      const updatedChats = new Set<string>();
-
-      for (const { envelope } of signalEvents) {
-        const { sourceNumber, sourceName, dataMessage, timestamp } = envelope;
-        if (dataMessage) {
-          if (dataMessage.message === null) {
-            // console.warn(
-            //   `Null message in envelope:\n${JSON.stringify(envelope, null, 2)}`
-            // );
-            continue;
-          }
-
-          const chatId = dataMessage.groupInfo?.groupId || sourceNumber;
-          updatedChats.add(chatId);
-
-          getMessages(chatId).push({
-            sourceNumber,
-            sourceName,
-            timestamp,
-            content: dataMessage.message ?? "",
-          });
+        let chat = chats.get(chatId);
+        if (!chat) {
+          const group = groupLookup.get(chatId);
+          chats.set(chatId, (chat = new Chat(chatId, group).start()));
         }
+
+        chat.addEvent(event);
       }
-
-      if (!updatedChats.size) continue;
-
-      await Promise.all(
-        [...updatedChats].map(async (chatId) => {
-          const chatMessages = [
-            new SystemChatMessage(
-              `You are a helpful and friendly assistant named ${agentName}. You are on a first name basis with everyone in the chat. You always answer factually to the best of your ability and never make things up. Some of the conversations in the chat do not involve you; if you are not being addressed should respond with exactly this as the entirety of your response: "${NO_RESPONSE}". It costs money when you respond, so use your best judgement.`
-            ),
-            ...getMessages(chatId)
-              .sort((a, b) => a.timestamp - b.timestamp)
-              .map(({ sourceNumber, sourceName, content }) => {
-                const message =
-                  sourceNumber === agentPhoneNumber
-                    ? new AIChatMessage(content)
-                    : new HumanChatMessage(
-                        formatChatMessage(sourceName, content)
-                      );
-                // message.name = sourceNumber;
-                return message;
-              }),
-            // new HumanChatMessage(formatChatMessage(agentName)),
-          ];
-
-          console.log(
-            `\n${chatId}\n===\n`,
-            JSON.stringify(
-              chatMessages.map((msg) => ({
-                source: msg.name,
-                message: msg.text,
-              })),
-              null,
-              2
-            ),
-            "\n==="
-          );
-
-          const model = new ChatOpenAI({
-            openAIApiKey,
-            temperature: 0,
-            modelName: "gpt-3.5-turbo",
-          });
-
-          console.log(`[${chatId}] Thinking...`);
-
-          let agentMessage: string;
-
-          try {
-            const response = await model.call(chatMessages);
-            agentMessage = response.text;
-          } catch (e) {
-            console.error("Error while consulting LLM", e);
-            return;
-          }
-
-          console.log(`[${chatId}] Decided on message`);
-
-          let timestamp: number | undefined;
-          if (agentMessage === NO_RESPONSE) {
-            console.log("No response.");
-            timestamp = Date.now();
-          } else {
-            timestamp = await sendMessage({
-              number: agentPhoneNumber,
-              recipients: [
-                groupLookup.get(chatId)?.id || chatId,
-                // "group.VHpiT29NL3VDbXJ0Y2R0cmw0Q0pRTUxzbml5UzBvTWd4dXpNVlZPdzE0OD0=",
-              ],
-              message: agentMessage,
-            });
-          }
-
-          if (!timestamp) {
-            console.log(`[${chatId}] No timestamp receieved`);
-            return;
-          }
-
-          getMessages(chatId).push({
-            sourceNumber: agentPhoneNumber,
-            sourceName: agentName,
-            timestamp,
-            content: agentMessage,
-          });
-
-          console.log(`[${chatId}] Responded`);
-        })
-      );
     } finally {
       await sleep(5000);
     }
   }
 }
-
-interface Message {
-  sourceNumber: string;
-  sourceName: string;
-  timestamp: number;
-  content: string;
-}
-
-const chats = new Map<string, Message[]>();
-
-function getMessages(chatId: string) {
-  let messages = chats.get(chatId);
-  if (!messages) chats.set(chatId, (messages = []));
-  return messages;
-}
-
-function formatChatMessage(sourceName: string, content = "") {
-  return `FROM: ${sourceName}\nMESSAGE: ${content}`;
-}
-
-const NO_RESPONSE = "%__NO_RESPONSE__%";
