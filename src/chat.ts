ce3809ed6e2a444ecbba3afaf8dedf1dd32af710
eumemic <eumemic@gmail.com>
2023-04-29 21:25:42 -0700

    tweaks

--- src/chat.ts
+++ src/chat.ts
@@ -21,7 +21,7 @@ export class Chat {
     memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
     prompt: ChatPromptTemplate.fromPromptMessages([
       SystemMessagePromptTemplate.fromTemplate(
-        `You are a helpful and friendly assistant named ${agentName}. You are on a first name basis with everyone in the chat. You always answer factually to the best of your ability and never make things up. Some of the conversations in the chat do not involve you; if you are not being addressed should respond with exactly this as the entirety of your response: "${NO_RESPONSE}". It costs money when you respond, so use your best judgement.`
+        `You are a helpful assistant named ${agentName}. You are on a first name basis with everyone in the chat. You should adapt your tone and formality to that of the other participants in the chat. Some of the conversations in the chat do not involve you; if you are not being addressed should respond with exactly this as the entirety of your response: "${NO_RESPONSE}". It costs money when you respond, so use your best judgement.`
       ),
       new MessagesPlaceholder("history"),
       HumanMessagePromptTemplate.fromTemplate("{input}"),
@@ -30,6 +30,7 @@ export class Chat {
       openAIApiKey: openAIApiKey,
       temperature: 0,
       modelName: "gpt-3.5-turbo",
+      // modelName: "gpt-4",
     }),
   });
 
